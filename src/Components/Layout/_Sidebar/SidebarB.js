02fcd0e07842985f6cbc228f97a4201b214948c7
Zorous <oussamabelhadi5@gmail.com>
2023-01-21 20:15:17 +0100

    sidebar changes

--- src/Components/Layout/_Sidebar/SidebarB.js
+++ src/Components/Layout/_Sidebar/SidebarB.js
@@ -20,14 +20,14 @@ const [Username,setUsername] =  useState("User")
     window.localStorage.removeItem("token")
     window.location.reload(false); 
   }
+  // <SidebarButton to="/player" icon={<i class="fa-solid fa-play"></i>}  title="Player" />
+  // <SidebarButton to="/playlists" icon={<i class="fa-solid fa-compact-disc"></i>}  title="Playlists" />
   return (
     <div className="sidebar-containerB">
       <img src={userimage}
         className='profile-img' alt='profile' /> 
       <div>
        <SidebarButton to="/main" icon={<i class="fa-solid fa-house"></i>}  title="Home" />
-       <SidebarButton to="/player" icon={<i class="fa-solid fa-play"></i>}  title="Player" />
-       <SidebarButton to="/playlists" icon={<i class="fa-solid fa-compact-disc"></i>}  title="Playlists" />
        <SidebarButton to="/search" icon={<i class="fa-solid fa-magnifying-glass"></i>}  title="Search" />
       </div>
       <div onClick={logout}>
